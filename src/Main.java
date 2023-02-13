1df9664cf53fba47b7eb563a66f237a653926920
ksaracevic1 <ksaracevic1@etf.unsa.ba>
2023-02-13 11:19:57 +0100

    novi commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world  13 02!");
+        System.out.println("Hello world  13 02   11:19!");
+
+
     }
 }
